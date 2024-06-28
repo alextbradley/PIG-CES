38d89adfe4d9ff7717249dc7c17caf13c85f3f95
Alex Bradley <alextbradley.1@gmail.com>
2024-06-28 12:34:18 +0100

    fix problem with output trajectories

--- observations/output_csv_observations.m
+++ observations/output_csv_observations.m
@@ -5,6 +5,7 @@
 %                (3) of grounded volume in 2015, releative to mass in 2015
 % "sd.csv": standard deviation of these observations
 % "truth_actual.csv": raw values of the grounding line position and mass
+% "truth_actual_cts.csv": raw values of the grounding line position and mass using the cts grounding line position
 % "truth_times.csv": corresponding times at which they are measured
 % "noise.csv": = [1,1,1] the noise in these observations, scaled by the sd
 
@@ -28,18 +29,22 @@ dx = 3e3;
 dy = 3e3;
 volume_2015 = sum(sum(data_2015.h .* data_2015.grfrac))*dx*dy;
 gl_pos_2015 = get_gl_pos(yy,grfrac_2015,x0);
+gl_pos_2015_cts = get_gl_pos_cts(yy,grfrac_2015,x0);
 
 data_1930 = load("steady_ridge_state.mat");
 grfrac_1930 = data_1930.grfrac;
 gl_pos_1930 = get_gl_pos(yy,grfrac_1930,x0);
+gl_pos_1930_cts = get_gl_pos_cts(yy,grfrac_1930,x0);
 
 data = [gl_pos_1930, gl_pos_2015, volume_2015];
+data_cts = [gl_pos_1930_cts, gl_pos_2015_cts, volume_2015];
 data_normalized = [0,0,0];
 
 noise_actual = [4500,4500,1e13]; %stds in these obs
 noise = ones(size(data));
 
 writematrix(data, 'truth_actual.csv');
+writematrix(data_cts, 'truth_actual_cts.csv');
 writematrix(data_normalized, 'truth.csv')
 writematrix(noise, 'noise.csv');
 writematrix(noise_actual, 'noise.csv');
